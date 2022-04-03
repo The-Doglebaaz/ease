13f6a6e0956278b07a2ca79d88925248f76e2c5d
Maanas <maanasmsingh@gmail.com>
2022-04-04 00:52:27 +0530

    shifted from postgreSQL to sqlite

--- ease/ease/settings.py
+++ ease/ease/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import keys
+import ease.keys as keys
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,18 +89,18 @@ WSGI_APPLICATION = 'ease.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_NAME'),
-            'USER': os.environ.get('POSTGRES_USER'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': 'db',
-            'PORT': 5432,
-        }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': os.environ.get('POSTGRES_NAME'),
+    #         'USER': os.environ.get('POSTGRES_USER'),
+    #         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+    #         'HOST': 'db',
+    #         'PORT': 5432,
+    #     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
