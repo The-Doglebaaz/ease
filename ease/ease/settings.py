385c6e81553b615eb2b9d71d64a977a1201b9c42
Maanas <maanasmsingh@gmail.com>
2022-04-04 09:58:24 +0000

    hosted

--- ease/ease/settings.py
+++ ease/ease/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = keys.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.232.183.89',]
 
 
 # Application definition
