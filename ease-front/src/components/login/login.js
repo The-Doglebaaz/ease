77450c10823ee71f2f1c46d6b38772ab9d92a784
Sarthak Aggarwal <getsarthakaggarwal@gmail.com>
2022-04-07 09:44:23 -0300

    login styling

--- ease-front/src/components/login/login.js
+++ ease-front/src/components/login/login.js
@@ -1,13 +1,17 @@
 import "./login.css";
 import {signInWithGoogle} from "./firebase.js"
+import GoogleButton from 'react-google-button'
+
 
 function Login(){
     return (
-    <div className="Login"> Signup/Login page!
-    <button onClick={signInWithGoogle}>Sign in with Google!</button>
-    <h1>{localStorage.getItem("name")}</h1>
-    <h1>{localStorage.getItem("email")}</h1>
-    <img src = {localStorage.getItem("profilePicture")}></img>
+    <div className="Login"> 
+    
+    <GoogleButton className="GoogleButton" onClick={ signInWithGoogle }
+/>
+    <h1 className="info">{localStorage.getItem("name")}</h1>
+    <h1 className="info">{localStorage.getItem("email")}</h1>
+    <img className="info" src = {localStorage.getItem("profilePicture")}></img>
      </div>);
 }
 
